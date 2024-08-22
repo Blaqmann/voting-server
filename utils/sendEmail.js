cb5348c3ae2d7a5c0891e4c56531f21b512ea662
Oluwaseyi Akintoye <seyiakintoye7@gmail.com>
2024-08-22 18:13:24 +0100

    Add files via upload

---
+++ utils/sendEmail.js
@@ -0,0 +1,25 @@
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+// Configure API key authorization: api-key
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+const sendEmail = async (options) => {
+   let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+   let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+   sendSmtpEmail.to = [{ email: options.email }];
+   sendSmtpEmail.sender = { email: 'seyiakintoye7@gmail.com', name: 'Blockchain Voting' }; // Replace with your verified sender
+   sendSmtpEmail.subject = options.subject;
+   sendSmtpEmail.textContent = options.message;
+
+   try {
+      await apiInstance.sendTransacEmail(sendSmtpEmail);
+   } catch (error) {
+      console.error('Error sending email:', error);
+   }
+};
+
+
+module.exports = sendEmail;
